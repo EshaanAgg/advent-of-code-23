1fa5b80bca71a2f38a59309315600e52d934db27
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-12-03 02:18:11 +0530

    day2: modify OCaml solution to use lists and split_on_char

--- ocaml/bin/d2p2.ml
+++ ocaml/bin/d2p2.ml
@@ -12,55 +12,44 @@ type ball =
   | Green of int
 
 let get_ball s =
-  let space_index = String.index s ' ' in
-  let ball_type = String.sub s (space_index + 1) (String.length s - space_index - 1) in
-  let ball_value = int_of_string (String.sub s 0 space_index) in
-  match ball_type with
-    | "red" -> Red ball_value
-    | "blue" -> Blue ball_value
-    | "green" -> Green ball_value
-    | _ -> failwith "Invalid ball type"
+  match String.split_on_char ' ' s with
+  | count :: typ :: _ -> 
+    let ball_value = int_of_string count in
+    begin match typ with
+      | "red" -> Red ball_value
+      | "blue" -> Blue ball_value
+      | "green" -> Green ball_value
+      | _ -> failwith "Invalid ball type"
+    end
+  | _ -> failwith "Invalid ball"
 
-let get_set s = 
-  let rec aux s init_set = 
-    let len = String.length s in
-    let comma_index = match (String.index s ',' ) with
-      | exception Not_found -> len 
-      | ind -> ind
+let get_set s =
+  let rec aux l init_set = 
+    match l with
+    | [] -> init_set
+    | h :: t -> 
+      let ball = get_ball (String.trim h) in
+      let upd_ball = match ball with
+        | Red i -> {init_set with red = max init_set.red i}
+        | Blue i -> {init_set with blue = max init_set.blue i}
+        | Green i -> {init_set with green = max init_set.green i}
+      in
+      aux t upd_ball
     in
-    let ball = get_ball (String.sub s 0 comma_index) in
-    let upd_ball = match ball with
-      | Red i -> {init_set with red = max init_set.red i}
-      | Blue i -> {init_set with blue = max init_set.blue i}
-      | Green i -> {init_set with green = max init_set.green i}
-    in 
-    let next_ind = comma_index + 2 in
-    if next_ind >= len 
-      then upd_ball
-      else aux (String.sub s next_ind (len - next_ind)) upd_ball 
-    in
-  aux s {red = 0; green = 0; blue = 0}
+  aux (String.split_on_char ',' s) {red = 0; green = 0; blue = 0}
 
 let get_min_set s = 
-  let rec aux s curr_set =
-    let len = String.length s in
-    let split_index = match (String.index s ';') with 
-      | exception Not_found -> len
-      | ind -> ind
-    in 
-    let next_set = get_set (String.sub s 0 split_index) in
-    let upd_set = {
-      red = max curr_set.red next_set.red;
-      green = max curr_set.green next_set.green;
-      blue = max curr_set.blue next_set.blue;
-    } in
-    let next_ind = split_index + 2 in
-    if next_ind >= len 
-      then upd_set
-      else aux (String.sub s next_ind (len - next_ind)) upd_set
-    in 
-  aux s {red = 0; green = 0; blue = 0}
-
+  let rec aux l curr_set = 
+    match l with 
+    | [] -> curr_set
+    | h :: t -> aux t {
+      red = max h.red curr_set.red;
+      green = max h.green curr_set.green;
+      blue = max h.blue curr_set.blue;
+    }
+  in 
+  let l = List.map (fun t -> get_set (String.trim t)) (String.split_on_char ';' s) in 
+  aux l {red = 0; green = 0; blue = 0}
     
 let rec solve_part_1 channel curr_ans = 
   match input_line channel with
